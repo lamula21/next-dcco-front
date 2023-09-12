1c1e64af902766df92b315ba6a663816a8dd3165
lamula21 <jvaldiv8@montgomerycollege.edu>
2023-09-11 21:33:52 -0400

    added-fixed: text-left, ul>li

--- app/(routes-page)/(events-training)/calendar/Calendar.jsx
+++ app/(routes-page)/(events-training)/calendar/Calendar.jsx
@@ -120,127 +120,121 @@ export function Calendar({ events }) {
 														{date.date()}
 													</h1>
 												</div>
-												{events.map((event, index) => {
-													return (
-														<ul key={event._id} className="flex flex-col gap-1">
-															{/* One Event Document */}
-															{date
-																.format('MMMM Do, YYYY')
-																.includes(event.date) && index < 2 ? (
-																<>
-																	<Dialog>
-																		<TooltipProvider delayDuration={400}>
-																			<Tooltip>
-																				<TooltipTrigger>
-																					<DialogTrigger asChild>
-																						<li className="px-2 py-1 flex rounded-md text-xs text-[10px] font-semibold items-center gap-1 transition-colors hover:bg-gray-950 hover:rounded-md">
-																							<h1 className="text-[#9E9589] text-clip overflow-hidden">
-																								<span className="inline-flex w-2 h-2 bg-yellow-700 rounded-full mr-1" />
-																								{event.init_time} -{' '}
-																								{event.end_time}{' '}
-																								<span className="ml-[2px] text-[#D6D3CD]">
-																									{event.title}
-																								</span>
-																							</h1>
-																						</li>
-																					</DialogTrigger>
-																				</TooltipTrigger>
-																				<TooltipContent className="border border-white/20 bg-gray-950">
-																					<p className="text-xs">See Event</p>
-																				</TooltipContent>
-																			</Tooltip>
-																		</TooltipProvider>
+												<ul className="flex flex-col gap-1">
+													{events.map((event, index) => {
+														return date
+															.format('MMMM Do, YYYY')
+															.includes(event.date) && index < 2 ? (
+															<Dialog key={event._id}>
+																<TooltipProvider delayDuration={400}>
+																	<Tooltip>
+																		<TooltipTrigger>
+																			<DialogTrigger asChild>
+																				<li className="px-2 py-1 flex rounded-md text-xs text-left font-semibold items-center gap-1 transition-colors hover:bg-gray-950 hover:rounded-md">
+																					<h1 className="text-[#9E9589] text-clip overflow-hidden">
+																						<span className="inline-flex w-2 h-2 bg-yellow-700 rounded-full mr-1" />
+																						{event.init_time} - {event.end_time}{' '}
+																						<span className="ml-[2px] text-[#D6D3CD]">
+																							{event.title}
+																						</span>
+																					</h1>
+																				</li>
+																			</DialogTrigger>
+																		</TooltipTrigger>
+																		<TooltipContent className="border border-white/20 bg-gray-950">
+																			<p className="text-xs">See Event</p>
+																		</TooltipContent>
+																	</Tooltip>
+																</TooltipProvider>
 
-																		<DialogContent className="sm:max-w-4xl px-20 py-10 h-screen">
-																			<DialogHeader className="overflow-y-scroll">
+																<DialogContent className="sm:max-w-4xl px-20 py-10 h-screen">
+																	<DialogHeader className="overflow-y-scroll">
+																		{event.imageUrl[0] ? (
+																			<div className="self-center my-10">
 																				{event.imageUrl[0] ? (
-																					<div className="self-center my-10">
-																						{event.imageUrl[0] ? (
-																							<NextuiImage
-																								isBlurred
-																								isZoomed
-																								src={event.imageUrl[0]}
-																								alt="event image"
-																								width={650}
-																								className="max-w-[650px] h-[20rem] object-cover rounded-3xl"
-																							/>
-																						) : null}
-																					</div>
+																					<NextuiImage
+																						isBlurred
+																						isZoomed
+																						src={event.imageUrl[0]}
+																						alt="event image"
+																						width={650}
+																						className="max-w-[650px] h-[20rem] object-cover rounded-3xl"
+																					/>
 																				) : null}
+																			</div>
+																		) : null}
 
-																				<DialogTitle className="my-5 flex flex-col gap-1">
-																					<span className="text-[#C07331] text-lg tracking-wider font-normal">
-																						{event.date} @ {event.init_time}{' '}
-																						{event.init_time.split(':')[0] >= 12
-																							? 'PM'
-																							: 'AM'}{' '}
-																						- {event.end_time}{' '}
-																						{event.end_time.split(':')[0] >= 12
-																							? 'PM'
-																							: 'AM'}
-																					</span>
+																		<DialogTitle className="my-5 flex flex-col gap-1">
+																			<span className="text-[#C07331] text-lg tracking-wider font-normal">
+																				{event.date} @ {event.init_time}{' '}
+																				{event.init_time.split(':')[0] >= 12
+																					? 'PM'
+																					: 'AM'}{' '}
+																				- {event.end_time}{' '}
+																				{event.end_time.split(':')[0] >= 12
+																					? 'PM'
+																					: 'AM'}
+																			</span>
 
-																					<span className="text-5xl font-bold tracking-tight">
-																						{event.title}
-																					</span>
+																			<span className="text-5xl font-bold tracking-tight">
+																				{event.title}
+																			</span>
 
-																					<span className="text-base tracking-normal font-light my-4">
-																						{/* {event.subtitle} */}
-																						{`This is a mocked up SUBTITLE. Join
+																			<span className="text-base tracking-normal font-light my-4">
+																				{/* {event.subtitle} */}
+																				{`This is a mocked up SUBTITLE. Join
 																						us for the General Meeting, an
 																						online gathering where we'll discuss
 																						everything and anything in a casual
 																						and relaxed atmosphere.`}
+																			</span>
+																		</DialogTitle>
+
+																		<div className="flex flex-col gap-5">
+																			<div>
+																				<span className="text-2xl font-semibold">
+																					About this event
+																				</span>
+																			</div>
+
+																			<div className="flex gap-28">
+																				<div className="flex gap-2 items-center">
+																					<i className="bx bx-time block text-[#468CE5] p-2 bg-[#1C1E1F] rounded-lg" />
+																					<span>
+																						{getDifferenceTime(
+																							event.init_time,
+																							event.end_time
+																						)}
+																						{' minutes'}
 																					</span>
-																				</DialogTitle>
-
-																				<div className="flex flex-col gap-5">
-																					<div>
-																						<span className="text-2xl font-semibold">
-																							About this event
-																						</span>
-																					</div>
-
-																					<div className="flex gap-28">
-																						<div className="flex gap-2 items-center">
-																							<i className="bx bx-time block text-[#468CE5] p-2 bg-[#1C1E1F] rounded-lg" />
-																							<span>
-																								{getDifferenceTime(
-																									event.init_time,
-																									event.end_time
-																								)}
-																								{' minutes'}
-																							</span>
-																						</div>
-
-																						<div className="flex gap-2 items-center">
-																							<i className="bx bx-calendar-event text-[#468CE5] p-2 bg-[#1C1E1F] rounded-lg"></i>
-																							<span>{event.address}</span>
-																						</div>
-																					</div>
-
-																					<DialogDescription className="text-xl text-white">
-																						{event.description}
-																					</DialogDescription>
 																				</div>
-																			</DialogHeader>
 
-																			<DialogFooter>
-																				<a
-																					href="https://www.eventbrite.com/e/general-meeting-tickets-716946756047?aff=oddtdtcreator"
-																					target="_blank"
-																					className="flex-none rounded-md h-10 bg-[#C07331] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-700"
-																				>
-																					Reserve a spot
-																				</a>
-																			</DialogFooter>
-																		</DialogContent>
-																	</Dialog>
-																</>
-															) : null}
-														</ul>
-													)
-												})}
+																				<div className="flex gap-2 items-center">
+																					<i className="bx bx-calendar-event text-[#468CE5] p-2 bg-[#1C1E1F] rounded-lg"></i>
+																					<span>{event.address}</span>
+																				</div>
+																			</div>
+
+																			<DialogDescription className="text-xl text-white">
+																				{event.description}
+																			</DialogDescription>
+																		</div>
+																	</DialogHeader>
+
+																	<DialogFooter>
+																		<a
+																			href="https://www.eventbrite.com/e/general-meeting-tickets-716946756047?aff=oddtdtcreator"
+																			target="_blank"
+																			className="flex-none rounded-md h-10 bg-[#C07331] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-700"
+																		>
+																			Reserve a spot
+																		</a>
+																	</DialogFooter>
+																</DialogContent>
+															</Dialog>
+														) : null
+													})}
+												</ul>
 												{events.length - 3 > 0 ? (
 													<span className="px-2">{events.length - 3} more</span>
 												) : null}
