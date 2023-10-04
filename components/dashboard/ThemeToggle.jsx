9390308dcdcdc20a40e1209cf653b53d44819774
lamula21 <jvaldiv8@montgomerycollege.edu>
2023-10-04 01:02:34 -0400

    fixed toggle icon

--- components/dashboard/ThemeToggle.jsx
+++ components/dashboard/ThemeToggle.jsx
@@ -31,8 +31,14 @@ export function ThemeToggle() {
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
 				<Button variant="ghost" size="sm" className="h-8 w-8 px-0">
-					<Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-					<Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+					{theme === 'dashboard' ? (
+						<GemIcon className="h-4 w-4" />
+					) : theme === 'light' ? (
+						<Sun className="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+					) : (
+						<Moon className="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+					)}
+
 					<span className="sr-only">Toggle theme</span>
 				</Button>
 			</DropdownMenuTrigger>
